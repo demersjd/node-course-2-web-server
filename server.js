2415071dc551c849510169f6b0673b701ac544cf
Justin <justin.demers@hotmail.com>
2019-01-12 16:01:23 -0600

    Set up start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -53,6 +54,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up and doing some running on the port 3000, Good job!');
+app.listen(port, () => {
+    console.log(`Server is up and doing some running on the port ${port}, Good job!`);
 });
